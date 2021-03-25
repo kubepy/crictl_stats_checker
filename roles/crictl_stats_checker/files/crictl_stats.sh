e3f0d796b74333e5701bd7c9fda75d47ac084b9e
root <root@bastion.ocp4.example.com>
2021-03-25 20:50:01 +0800

    update

---
+++ roles/crictl_stats_checker/files/crictl_stats.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+crictl_stats=$(crictl stats --output json)
+echo "$crictl_stats" | jq -r '.stats[] | "\(.attributes.labels."io.kubernetes.pod.namespace") \(.attributes.labels."io.kubernetes.pod.name") \(.attributes.metadata.name) \(.attributes.id[:13]) \(.memory.workingSetBytes.value|tonumber/1000/1000)Mb \(.writableLayer.usedBytes.value|tonumber/1000)Kb"' | column -t
