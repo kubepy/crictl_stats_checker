ccf1dc19baa65f2681847c41a1cbd740ed62d562
root <root@bastion.ocp4.example.com>
2021-03-25 23:27:41 +0800

    update crictl customize command

--- roles/crictl_stats_checker/files/crictl_stats.sh
+++ roles/crictl_stats_checker/files/crictl_stats.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-crictl_stats=$(crictl stats --output json)
-echo "$crictl_stats" | jq -r '.stats[] | "\(.attributes.labels."io.kubernetes.pod.namespace") \(.attributes.labels."io.kubernetes.pod.name") \(.attributes.metadata.name) \(.attributes.id[:13]) \(.memory.workingSetBytes.value|tonumber/1000/1000)Mb \(.writableLayer.usedBytes.value|tonumber/1000)Kb"' | column -t
+/tmp/crictl stats | tail -n +2
+#crictl_stats=$(crictl stats --output json)
+#echo "$crictl_stats" | jq -r '.stats[] | "\(.attributes.labels."io.kubernetes.pod.namespace") \(.attributes.labels."io.kubernetes.pod.name") \(.attributes.metadata.name) \(.attributes.id[:13]) \(.memory.workingSetBytes.value|tonumber/1000/1000)Mb \(.writableLayer.usedBytes.value|tonumber/1000)Kb"' | column -t
